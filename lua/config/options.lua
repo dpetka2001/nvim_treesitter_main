8f63131565a66e7402964497cee021359d99e08b
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2025-06-21 21:49:22 +0300

    diff for `nvim-treesitter` main branch

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- by @dpetka2001
+-- needed to make sure the spec of the Extra gets added first, so other Extras can add onto it
+vim.g.xtras_prios = {
+  ["plugins.extras.ui.treesitter-rewrite"] = 2,
+}
